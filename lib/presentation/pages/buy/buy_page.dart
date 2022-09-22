d93033ed065c558999fa4a0679f1d2ef26b5af1d
Umidjon Yoqubov <umidjonxomidjonovich@gmail.com>
2022-09-22 16:26:58 +0500

    created buy_page

---
+++ lib/presentation/pages/buy/buy_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class BuyPage extends StatefulWidget {
+  const BuyPage({super.key});
+
+  @override
+  State<BuyPage> createState() => _BuyPageState();
+}
+
+class _BuyPageState extends State<BuyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
